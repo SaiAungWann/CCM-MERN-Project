43b3f8d899086bb0ad5cced9d2d8656075d4c5ee
SaiAungWann <mrsaiaungwann@gmail.com>
2024-07-02 17:46:27 +0700

    get-all-api-and-singal-api

--- backend/controller/RecipeController.js
+++ backend/controller/RecipeController.js
@@ -1,6 +1,7 @@
 const Recipe = require("../model/Recipe");
 const RecipeController = {
-  index: (req, res) => {
+  index: async (req, res) => {
+    let recipe = await Recipe.find().sort({ createdAt: -1 });
     return res.json({ message: "Get all recipes" });
   },
   store: async (req, res) => {
@@ -17,8 +18,14 @@ const RecipeController = {
       return res.status(400).json({ message: "Something went wrong" });
     }
   },
-  show: (req, res) => {
-    return res.json({ message: "Get single recipes" });
+  show: async (req, res) => {
+    try {
+      let id = req.params.id;
+      let recipe = await Recipe.findById(id);
+      return res.json(recipe);
+    } catch (error) {
+      return res.status(404).json({ message: "Recipe Not found" });
+    }
   },
   destory: (req, res) => {
     return res.json({ message: "delete recipes" });
