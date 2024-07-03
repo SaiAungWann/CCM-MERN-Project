f7066c992a6a3874bc7333753996f5052f0a8eae
SaiAungWann <mrsaiaungwann@gmail.com>
2024-07-03 15:16:56 +0700

    express-validation

---
+++ backend/middleware/handleErrorMessage.js
@@ -0,0 +1,13 @@
+const { validationResult } = require("express-validator");
+
+const handleErrorMessage = (req, res, next) => {
+  const result = validationResult(req);
+
+  if (!result.isEmpty()) {
+    return res.status(400).json({ error: mapped() });
+  } else {
+    next();
+  }
+};
+
+module.exports = handleErrorMessage;
--- backend/routes/Recipes.js
+++ backend/routes/Recipes.js
@@ -1,11 +1,20 @@
 const express = require("express");
-
-const app = express();
+const handleErrorMessage = require("../middleware/handleErrorMessage");
+const { body } = require("express-validator");
 const RecipeController = require("../controller/RecipeController");
 const router = express.Router();
 
 router.get("", RecipeController.index);
-router.post("", RecipeController.store);
+router.post(
+  "",
+  [
+    body("title").notEmpty,
+    body("description").notEmpty,
+    body("ingredients").isArray({ min: 3 }).notEmpty,
+  ],
+  handleErrorMessage,
+  RecipeController.store
+);
 router.get("/:id", RecipeController.show);
 router.delete("/:id", RecipeController.destory);
 router.patch("/:id", RecipeController.update);
